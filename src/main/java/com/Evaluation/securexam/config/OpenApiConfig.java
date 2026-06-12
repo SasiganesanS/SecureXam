95503582fa90d0a7b96fa345985d3494eec8b91b
SasiganesanS <sasiganesh7421@gmail.com>
2026-06-12 12:42:25 +0530

    Swager JWT Token and header clear

--- src/main/java/com/Evaluation/securexam/config/OpenApiConfig.java
+++ src/main/java/com/Evaluation/securexam/config/OpenApiConfig.java
@@ -1,10 +1,20 @@
 package com.Evaluation.securexam.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "SecureXam API",
+                version = "1.0"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
